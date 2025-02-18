1e1f65c8c5d1197e8a87cd966a2e07c388ffab30
piyushgoyall <piyushgoyal3003@gmail.com>
2025-02-18 06:53:13 -0500

    new

--- service/__init__.py
+++ service/__init__.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 # This must be imported after the Flask app is created
 from service import routes # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers # pylint: disable=wrong-import-position
+from service.common import log_handlers  # pylint: disable=wrong-import-position
 
 log_handlers.init_logging(app, "gunicorn.error")
 
-app.logger.info(70 * "*")
+# Fix for E261: Ensure at least two spaces before the inline comment
+stars = "*" * 70
+app.logger.info(stars)
 app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
-app.logger.info(70 * "*")
+app.logger.info(stars)
